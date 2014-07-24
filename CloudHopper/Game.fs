b61dea47c1089f9c0a0f6c84bc39c4d348d2dda9
John Stovin <john.d.stovin@gmail.com>
2014-07-24 11:12:34 +0100

    Basic Game

---
+++ CloudHopper/Game.fs
@@ -0,0 +1,17 @@
+﻿module CloudHopper
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type CloudHopperGame () as g =
+    inherit Game()
+
+    let graphics = new GraphicsDeviceManager(g)
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    override g.LoadContent () =
+        spriteBatch <- new SpriteBatch(g.GraphicsDevice)
+
+    override g.Draw gametime =
+        g.GraphicsDevice.Clear Color.CornflowerBlue
+        base.Draw gametime
