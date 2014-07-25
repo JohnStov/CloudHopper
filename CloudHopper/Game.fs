ab65eec57b6e495102a890ef136c0a2dd7ed62e5
John Stovin <john.d.stovin@gmail.com>
2014-07-25 14:22:22 +0100

    Gravity

--- CloudHopper/Game.fs
+++ CloudHopper/Game.fs
@@ -3,12 +3,14 @@
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Content
+open System
 
 type Actor =
     {
         Position : Vector2;
         Size : Vector2;
         Texture : Texture2D;
+        Velocity : Vector2;
     }
 
     member this.Bounds 
@@ -17,7 +19,7 @@ type Actor =
 let CreateActor (content:ContentManager) (textureName, position) = 
     let tex = content.Load<Texture2D> textureName
     let size = new Vector2 ((float32 tex.Width), (float32 tex.Height))
-    { Position = position; Size = size; Texture = tex }
+    { Position = position; Size = size; Texture = tex; Velocity = new Vector2(1.f, 0.f) }
 
 let DrawActor (sb:SpriteBatch) actor =
     sb.Draw (actor.Texture, actor.Position, Color.White)
@@ -25,11 +27,16 @@ let DrawActor (sb:SpriteBatch) actor =
 let (|InBounds|_|) (bounds : Rectangle) (a : Actor) =
    if bounds.Contains a.Bounds then Some(a) else None
 
-let MoveActor (bounds : Rectangle) x y (a : Actor) =
-    let newPos = { Position = new Vector2 (a.Position.X + x, a.Position.Y + y); Size = a.Size; Texture = a.Texture }
+let MoveActor (bounds : Rectangle) (a : Actor) =
+    let newPos = { a with Position = a.Position + a.Velocity }
     match newPos with
     | InBounds bounds newPos -> newPos
     | _ -> a
+
+let ApplyGravity (a : Actor) =
+    let g = 0.05
+    let downward = Math.Min(double a.Velocity.Y + g, 5.0)
+    { a with Velocity = new Vector2 (a.Velocity.X, float32 downward) }
 
 type CloudHopperGame () as g =
     inherit Game()
@@ -44,7 +51,7 @@ type CloudHopperGame () as g =
         actors <- actorData |> List.map (CreateActor g.Content)
 
     override g.Update _ =
-        actors <- actors |> List.map (MoveActor g.GraphicsDevice.Viewport.Bounds 1.f 0.f)
+        actors <- actors |> List.map ApplyGravity |> List.map (MoveActor g.GraphicsDevice.Viewport.Bounds)
 
     override g.Draw _ =
         g.GraphicsDevice.Clear Color.CornflowerBlue
